b392c56c93ab22fa6ececbb74feb9dcd70ca95c6
Steve Persch <steve.persch@pantheon.io>
2017-02-07 16:21:29 -0600

    Bringing over FeatureContext from example-drops-8-composer

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -6,6 +6,7 @@ use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 use Drupal\DrupalExtension\Context\MinkContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Hook\Scope\AfterStepScope;
 
 use Drupal\DrupalExtension\Context\RawDrupalContext;
 
@@ -29,8 +30,8 @@ class FeatureContext extends RawDrupalContext implements Context, SnippetAccepti
   /** @BeforeScenario */
   public function gatherContexts(BeforeScenarioScope $scope)
   {
-      $environment = $scope->getEnvironment();
-      $this->minkContext = $environment->getContext('Drupal\DrupalExtension\Context\MinkContext');
+    $environment = $scope->getEnvironment();
+    $this->minkContext = $environment->getContext('Drupal\DrupalExtension\Context\MinkContext');
   }
 
 //
@@ -44,163 +45,206 @@ class FeatureContext extends RawDrupalContext implements Context, SnippetAccepti
 //  }
 //
 
-    /**
-     * Fills in form field with specified id|name|label|value
-     * Example: And I enter the value of the env var "TEST_PASSWORD" for "edit-account-pass-pass1"
-     *
-     * @Given I enter the value of the env var :arg1 for :arg2
-     */
-    public function fillFieldWithEnv($value, $field)
+  /**
+   * Fills in form field with specified id|name|label|value
+   * Example: And I enter the value of the env var "TEST_PASSWORD" for "edit-account-pass-pass1"
+   *
+   * @Given I enter the value of the env var :arg1 for :arg2
+   */
+  public function fillFieldWithEnv($value, $field)
+  {
+    $this->minkContext->fillField($field, getenv($value));
+  }
+
+  /**
+   * @Given I wait for the progress bar to finish
+   */
+  public function iWaitForTheProgressBarToFinish() {
+    $this->iFollowMetaRefresh();
+  }
+
+  /**
+   * @Given I follow meta refresh
+   *
+   * https://www.drupal.org/node/2011390
+   */
+  public function iFollowMetaRefresh() {
+    while ($refresh = $this->getSession()->getPage()->find('css', 'meta[http-equiv="Refresh"]')) {
+      $content = $refresh->getAttribute('content');
+      $url = str_replace('0; URL=', '', $content);
+      $this->getSession()->visit($url);
+    }
+  }
+
+  /**
+   * @Given I have wiped the site
+   */
+  public function iHaveWipedTheSite()
+  {
+    $site = getenv('TERMINUS_SITE');
+    $env = getenv('TERMINUS_ENV');
+
+    passthru("terminus env:wipe $site.$env --yes");
+  }
+
+  /**
+   * @Given I have reinstalled
+   */
+  public function iHaveReinstalled()
+  {
+    $site = getenv('TERMINUS_SITE');
+    $env = getenv('TERMINUS_ENV');
+    $site_name = getenv('TEST_SITE_NAME');
+    $site_mail = getenv('ADMIN_EMAIL');
+    $admin_password = getenv('ADMIN_PASSWORD');
+
+    passthru("terminus --yes drush $site.$env -- --yes site-install standard --site-name=\"$site_name\" --site-mail=\"$site_mail\" --account-name=admin --account-pass=\"$admin_password\"'");
+  }
+
+  /**
+   * @Given I have run the drush command :arg1
+   */
+  public function iHaveRunTheDrushCommand($arg1)
+  {
+    $site = getenv('TERMINUS_SITE');
+    $env = getenv('TERMINUS_ENV');
+
+    $return = '';
+    $output = array();
+    exec("terminus drush $site.$env -- " . $arg1, $output, $return);
+    // echo $return;
+    // print_r($output);
+
+  }
+
+  /**
+   * @Given I have committed my changes with comment :arg1
+   */
+  public function iHaveCommittedMyChangesWithComment($arg1)
+  {
+    $site = getenv('TERMINUS_SITE');
+    $env = getenv('TERMINUS_ENV');
+
+    passthru("terminus --yes $site.$env env:commit --message='$arg1'");
+  }
+
+  /**
+   * @Given I have exported configuration
+   */
+  public function iHaveExportedConfiguration()
+  {
+    $site = getenv('TERMINUS_SITE');
+    $env = getenv('TERMINUS_ENV');
+
+    $return = '';
+    $output = array();
+    exec("terminus drush $site.$env -- config-export -y", $output, $return);
+  }
+
+  /**
+   * @Given I wait :seconds seconds
+   */
+  public function iWaitSeconds($seconds)
+  {
+    sleep($seconds);
+  }
+
+  /**
+   * @Given I wait :seconds seconds or until I see :text
+   */
+  public function iWaitSecondsOrUntilISee($seconds, $text)
+  {
+    $errorNode = $this->spin( function($context) use($text) {
+      $node = $context->getSession()->getPage()->find('named', array('content', $text));
+      if (!$node) {
+        return false;
+      }
+      return $node->isVisible();
+    }, $seconds);
+
+    // Throw to signal a problem if we were passed back an error message.
+    if (is_object($errorNode)) {
+      throw new Exception("Error detected when waiting for '$text': " . $errorNode->getText());
+    }
+  }
+
+  // http://docs.behat.org/en/v2.5/cookbook/using_spin_functions.html
+  // http://mink.behat.org/en/latest/guides/traversing-pages.html#selectors
+  public function spin ($lambda, $wait = 60)
+  {
+    for ($i = 0; $i <= $wait; $i++)
     {
-        $this->minkContext->fillField($field, getenv($value));
-    }
+      if ($i > 0) {
+        sleep(1);
+      }
 
-    /**
-     * @Given I wait for the progress bar to finish
-     */
-    public function iWaitForTheProgressBarToFinish() {
-      $this->iFollowMetaRefresh();
-    }
+      $debugContent = $this->getSession()->getPage()->getContent();
+      file_put_contents("/tmp/mink/debug-" . $i, "\n\n\n=================================\n$debugContent\n=================================\n\n\n");
 
-    /**
-     * @Given I follow meta refresh
-     *
-     * https://www.drupal.org/node/2011390
-     */
-    public function iFollowMetaRefresh() {
-      while ($refresh = $this->getSession()->getPage()->find('css', 'meta[http-equiv="Refresh"]')) {
-        $content = $refresh->getAttribute('content');
-        $url = str_replace('0; URL=', '', $content);
-        $this->getSession()->visit($url);
+      try {
+        if ($lambda($this)) {
+          return true;
+        }
+      } catch (Exception $e) {
+        // do nothing
+      }
+
+      // If we do not see the text we are waiting for, fail fast if
+      // we see a Drupal 8 error message pane on the page.
+      $node = $this->getSession()->getPage()->find('named', array('content', 'Error'));
+      if ($node) {
+        $errorNode = $this->getSession()->getPage()->find('css', '.messages--error');
+        if ($errorNode) {
+          return $errorNode;
+        }
+        $errorNode = $this->getSession()->getPage()->find('css', 'main');
+        if ($errorNode) {
+          return $errorNode;
+        }
+        return $node;
       }
     }
 
-    /**
-     * @Given I have wiped the site
-     */
-    public function iHaveWipedTheSite()
-    {
-        $site = getenv('TERMINUS_SITE');
-        $env = getenv('TERMINUS_ENV');
+    $backtrace = debug_backtrace();
 
-        passthru("terminus --yes --site=$site --env=$env site wipe");
+    throw new Exception(
+      "Timeout thrown by " . $backtrace[1]['class'] . "::" . $backtrace[1]['function'] . "()\n" .
+      $backtrace[1]['file'] . ", line " . $backtrace[1]['line']
+    );
+
+    return false;
+  }
+
+  /**
+   * @AfterStep
+   */
+  public function afterStep(AfterStepScope $scope)
+  {
+    // Do nothing on steps that pass
+    $result = $scope->getTestResult();
+    if ($result->isPassed()) {
+      return;
     }
 
-    /**
-     * @Given I have reinstalled :arg1
-     */
-    public function iHaveReinstalled($arg1)
-    {
-        $site = getenv('TERMINUS_SITE');
-        $env = getenv('TERMINUS_ENV');
-        passthru("terminus --yes --site=$site --env=$env drush '--yes site-install standard --site-name=\"$arg1\" --account-name=admin'");
-    }
+    // Otherwise, dump the page contents.
+    $session = $this->getSession();
+    $page = $session->getPage();
+    $html = $page->getContent();
+    $html = static::trimHead($html);
 
-    /**
-     * @Given I have run the drush command :arg1
-     */
-    public function iHaveRunTheDrushCommand($arg1)
-    {
-        $return = '';
-        $output = array();
-        exec('terminus drush "' . $arg1 . '"', $output, $return);
-        // echo $return;
-        // print_r($output);
+    print "::::::::::::::::::::::::::::::::::::::::::::::::\n";
+    print $html . "\n";
+    print "::::::::::::::::::::::::::::::::::::::::::::::::\n";
+  }
 
-    }
-
-    /**
-     * @Given I have committed my changes with comment :arg1
-     */
-    public function iHaveCommittedMyChangesWithComment($arg1)
-    {
-        $site = getenv('TERMINUS_SITE');
-        $env = getenv('TERMINUS_ENV');
-
-        passthru("terminus --yes --site=$site --env=$env site code commit --message='$arg1'");
-    }
-
-    /**
-     * @Given I have exported configuration
-     */
-    public function iHaveExportedConfiguration()
-    {
-        $return = '';
-        $output = array();
-        exec("terminus drush 'config-export -y'", $output, $return);
-    }
-
-    /**
-     * @Given I wait :seconds seconds
-     */
-    public function iWaitSeconds($seconds)
-    {
-        sleep($seconds);
-    }
-
-    /**
-     * @Given I wait :seconds seconds or until I see :text
-     */
-    public function iWaitSecondsOrUntilISee($seconds, $text)
-    {
-        $errorNode = $this->spin( function($context) use($text) {
-            $node = $context->getSession()->getPage()->find('named', array('content', $text));
-            if (!$node) {
-              return false;
-            }
-            return $node->isVisible();
-        }, $seconds);
-
-        // Throw to signal a problem if we were passed back an error message.
-        if (is_object($errorNode)) {
-          throw new Exception("Error detected when waiting for '$text': " . $errorNode->getText());
-        }
-    }
-
-    // http://docs.behat.org/en/v2.5/cookbook/using_spin_functions.html
-    // http://mink.behat.org/en/latest/guides/traversing-pages.html#selectors
-    public function spin ($lambda, $wait = 60)
-    {
-        for ($i = 0; $i <= $wait; $i++)
-        {
-            if ($i > 0) {
-              sleep(1);
-            }
-
-            $debugContent = $this->getSession()->getPage()->getContent();
-            file_put_contents("/tmp/mink/debug-" . $i, "\n\n\n=================================\n$debugContent\n=================================\n\n\n");
-
-            try {
-                if ($lambda($this)) {
-                    return true;
-                }
-            } catch (Exception $e) {
-                // do nothing
-            }
-
-            // If we do not see the text we are waiting for, fail fast if
-            // we see a Drupal 8 error message pane on the page.
-            $node = $this->getSession()->getPage()->find('named', array('content', 'Error'));
-            if ($node) {
-              $errorNode = $this->getSession()->getPage()->find('css', '.messages--error');
-              if ($errorNode) {
-                return $errorNode;
-              }
-              $errorNode = $this->getSession()->getPage()->find('css', 'main');
-              if ($errorNode) {
-                return $errorNode;
-              }
-              return $node;
-            }
-        }
-
-        $backtrace = debug_backtrace();
-
-        throw new Exception(
-            "Timeout thrown by " . $backtrace[1]['class'] . "::" . $backtrace[1]['function'] . "()\n" .
-            $backtrace[1]['file'] . ", line " . $backtrace[1]['line']
-        );
-
-        return false;
-    }
+  /**
+   * Remove everything in the '<head>' element except the
+   * title, because it is long and uninteresting.
+   */
+  protected static function trimHead($html)
+  {
+    $html = preg_replace('#\<head\>.*\<title\>#sU', '<head><title>', $html);
+    $html = preg_replace('#\</title\>.*\</head\>#sU', '</title></head>', $html);
+    return $html;
+  }
 }
--- installer-features/installer.feature
+++ installer-features/installer.feature
@@ -3,30 +3,12 @@ Feature: Installer
   As a website user
   I need to be able to install a Drupal site
 
-  Scenario: Installer is ready
+  Scenario: Install via Drush
+
+
     Given I have wiped the site
-    And I am on "/core/install.php"
-    Then I should see "Choose language"
-
-  Scenario: Language selection
-    Given I am on "/core/install.php"
-    And I press "Save and continue"
-    Then I should see "Select an installation profile"
-
-  Scenario: Profile selection
-    Given I am on "/core/install.php?langcode=en"
-    And I press "Save and continue"
-    And I wait for the progress bar to finish
-    Then I should see "Site name"
-
-  Scenario: Configure site
-    Given I am on "/core/install.php?langcode=en&profile=standard"
-    And I enter "Example Dot Com Test Site" for "edit-site-name"
-    And I enter "john.doe@example.com" for "edit-site-mail"
-    And I enter "admin" for "Username"
-    And I enter the value of the env var "ADMIN_PASSWORD" for "edit-account-pass-pass1"
-    And I enter the value of the env var "ADMIN_PASSWORD" for "edit-account-pass-pass2"
-    And I enter "john.doe@example.com" for "edit-account-mail"
-    And I press "Save and continue"
+    # For this to work, an automation script needs to have made settings.php
+    # Without it (it is not in drops-7 by default) the command fails.
+    When I have run the drush command "si -y"
     And I visit "/"
-    Then I should see "Welcome to Example Dot Com"
+    Then I should see "Welcome to Site-Install"
