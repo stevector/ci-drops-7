13de5e4f3427c1feb228f5de06eb35b2f12cd1bd
Steve Persch <steve.persch@pantheon.io>
2017-02-08 12:40:14 -0600

    Removing unused steps

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -34,17 +34,6 @@ class FeatureContext extends RawDrupalContext implements Context, SnippetAccepti
     $this->minkContext = $environment->getContext('Drupal\DrupalExtension\Context\MinkContext');
   }
 
-//
-// Place your definition and hook methods here:
-//
-//  /**
-//   * @Given I have done something with :stuff
-//   */
-//  public function iHaveDoneSomethingWith($stuff) {
-//    doSomethingWith($stuff);
-//  }
-//
-
   /**
    * Fills in form field with specified id|name|label|value
    * Example: And I enter the value of the env var "TEST_PASSWORD" for "edit-account-pass-pass1"
@@ -56,25 +45,6 @@ class FeatureContext extends RawDrupalContext implements Context, SnippetAccepti
     $this->minkContext->fillField($field, getenv($value));
   }
 
-  /**
-   * @Given I wait for the progress bar to finish
-   */
-  public function iWaitForTheProgressBarToFinish() {
-    $this->iFollowMetaRefresh();
-  }
-
-  /**
-   * @Given I follow meta refresh
-   *
-   * https://www.drupal.org/node/2011390
-   */
-  public function iFollowMetaRefresh() {
-    while ($refresh = $this->getSession()->getPage()->find('css', 'meta[http-equiv="Refresh"]')) {
-      $content = $refresh->getAttribute('content');
-      $url = str_replace('0; URL=', '', $content);
-      $this->getSession()->visit($url);
-    }
-  }
 
   /**
    * @Given I have wiped the site
@@ -115,104 +85,6 @@ class FeatureContext extends RawDrupalContext implements Context, SnippetAccepti
     // echo $return;
     // print_r($output);
 
-  }
-
-  /**
-   * @Given I have committed my changes with comment :arg1
-   */
-  public function iHaveCommittedMyChangesWithComment($arg1)
-  {
-    $site = getenv('TERMINUS_SITE');
-    $env = getenv('TERMINUS_ENV');
-
-    passthru("terminus --yes $site.$env env:commit --message='$arg1'");
-  }
-
-  /**
-   * @Given I have exported configuration
-   */
-  public function iHaveExportedConfiguration()
-  {
-    $site = getenv('TERMINUS_SITE');
-    $env = getenv('TERMINUS_ENV');
-
-    $return = '';
-    $output = array();
-    exec("terminus drush $site.$env -- config-export -y", $output, $return);
-  }
-
-  /**
-   * @Given I wait :seconds seconds
-   */
-  public function iWaitSeconds($seconds)
-  {
-    sleep($seconds);
-  }
-
-  /**
-   * @Given I wait :seconds seconds or until I see :text
-   */
-  public function iWaitSecondsOrUntilISee($seconds, $text)
-  {
-    $errorNode = $this->spin( function($context) use($text) {
-      $node = $context->getSession()->getPage()->find('named', array('content', $text));
-      if (!$node) {
-        return false;
-      }
-      return $node->isVisible();
-    }, $seconds);
-
-    // Throw to signal a problem if we were passed back an error message.
-    if (is_object($errorNode)) {
-      throw new Exception("Error detected when waiting for '$text': " . $errorNode->getText());
-    }
-  }
-
-  // http://docs.behat.org/en/v2.5/cookbook/using_spin_functions.html
-  // http://mink.behat.org/en/latest/guides/traversing-pages.html#selectors
-  public function spin ($lambda, $wait = 60)
-  {
-    for ($i = 0; $i <= $wait; $i++)
-    {
-      if ($i > 0) {
-        sleep(1);
-      }
-
-      $debugContent = $this->getSession()->getPage()->getContent();
-      file_put_contents("/tmp/mink/debug-" . $i, "\n\n\n=================================\n$debugContent\n=================================\n\n\n");
-
-      try {
-        if ($lambda($this)) {
-          return true;
-        }
-      } catch (Exception $e) {
-        // do nothing
-      }
-
-      // If we do not see the text we are waiting for, fail fast if
-      // we see a Drupal 8 error message pane on the page.
-      $node = $this->getSession()->getPage()->find('named', array('content', 'Error'));
-      if ($node) {
-        $errorNode = $this->getSession()->getPage()->find('css', '.messages--error');
-        if ($errorNode) {
-          return $errorNode;
-        }
-        $errorNode = $this->getSession()->getPage()->find('css', 'main');
-        if ($errorNode) {
-          return $errorNode;
-        }
-        return $node;
-      }
-    }
-
-    $backtrace = debug_backtrace();
-
-    throw new Exception(
-      "Timeout thrown by " . $backtrace[1]['class'] . "::" . $backtrace[1]['function'] . "()\n" .
-      $backtrace[1]['file'] . ", line " . $backtrace[1]['line']
-    );
-
-    return false;
   }
 
   /**
